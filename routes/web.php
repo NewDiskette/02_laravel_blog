4e393ec3aa60b4c105ff9981ee526b6eaca959d0
newdiskette <newdiskette@gmail.com>
2022-07-26 12:07:37 +0500

    lesson 3 (view"main.index")

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['namespace' => 'Main'], function (){
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
